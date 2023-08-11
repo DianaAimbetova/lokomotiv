5042bfc364cda1e9873989f3053853e85937f64d
diana_aimbetova@epam.com <diana_aimbetova@epam.com>
2023-08-11 12:24:02 +0600

    alter model

--- data/views.py
+++ data/views.py
@@ -20,11 +20,18 @@ def import_excel(request):
             parent_scheme = Scheme.objects.filter(number = dbframe.parent)
             if parent_scheme:
                 obj = Scheme.objects.create(name=dbframe.name, number=dbframe.number,
-                                            parent=parent_scheme[0])
+                                            parent=parent_scheme[0], chapter = dbframe.chapter, 
+                                            chineese_name = dbframe.chineese_name, quantity = dbframe.quantity
+                                            )
             else:
                  # first create parent
-                 parent_obj = obj = Scheme.objects.create(name='неизвестная схема с номером '+ dbframe.parent, number=dbframe.parent)  
-                 obj = Scheme.objects.create(name=dbframe.name, number=dbframe.number, parent = parent_obj)          
+                 parent_obj = obj = Scheme.objects.create(name='неизвестная схема с номером '+ dbframe.parent, number=dbframe.parent, 
+                                                        chapter = dbframe.chapter, chineese_name = dbframe.chineese_name, 
+                                                        quantity = dbframe.quantity) 
+                 # then create child 
+                 obj = Scheme.objects.create(name=dbframe.name, number=dbframe.number, parent = parent_obj,
+                                            chapter = dbframe.chapter, chineese_name = dbframe.chineese_name, 
+                                            quantity = dbframe.quantity)          
                  parent_obj.save() 
             obj.save()
         return render(request, 'data/Import_excel_db.html', {
--- treemenu/models.py
+++ treemenu/models.py
@@ -6,13 +6,16 @@ class Scheme(MPTTModel):
     name = models.CharField(max_length=50)
     number = models.CharField(max_length=50, blank=True, null=True)
     parent = TreeForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, related_name = 'children')
+    chineese_name = models.CharField(max_length=50, blank=True, null=True)
+    chapter = models.CharField(max_length=50, blank=True, null=True)
+    quantity = models.CharField(blank=True, null=True, max_length = 50)
 
     def __str__(self):
         return self.name
     
     class MPTTMeta:
         order_insertion_by = ['name']
-        
+
     class Meta:
         verbose_name = 'Схема'
         verbose_name_plural = 'Схемы'
